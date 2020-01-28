542db8f62d0299ee50c41369e62a3b38dc71b2b5
Viral Mehta <viral.mehta7@gmail.com>
2020-01-28 19:23:10 +0530

    added files

--- src/main/java/com/samples/HelloWorld.java
+++ src/main/java/com/samples/HelloWorld.java
@@ -5,6 +5,10 @@ import lombok.extern.flogger.Flogger;
 @Flogger
 public class HelloWorld {
     public static void main(String[] args) {
+        printHello();
+    }
+
+    private static void printHello() {
         log.atInfo().log("Hello world");
     }
 }
